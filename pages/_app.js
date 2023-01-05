d84bbac24fa77c9a73254235572a6ffdab45c8fc
Farhan <mansurifarhanfm@gmail.com>
2023-01-05 21:44:53 +0530

    Hi

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import BottomNavbar from '../components/BottomNavbar'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="">
+      {' '}
+      <Navbar />
+      <div className="p-[4rem]">
+        <Component {...pageProps} />
+        <BottomNavbar />
+      </div>
+    </div>
+  )
 }
